6c896bc0638fe92f1b0048dcc609531e82cbbad3
Florian Beeres <yuuki@protonmail.com>
2020-11-12 11:51:10 +0100

    Update

---
+++ stow/fish/.config/fish/fbs-work.fish
@@ -0,0 +1,5 @@
+contains (brew --prefix)/opt/coreutils/libexec/gnubin $PATH
+or set -x PATH (brew --prefix)/opt/coreutils/libexec/gnubin $PATH
+
+contains /opt/local/bin $PATH
+or set -x PATH /opt/local/bin $PATH
